6c5fc630b5f254a43ee46c091053bdd83fe1dbd0
rakaprakoso <62010062+rakaprakoso@users.noreply.github.com>
2021-07-15 02:43:12 +0700

    Prod + Minified

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,6 +1,8 @@
 const mix = require('laravel-mix');
 const tailwindcss = require('tailwindcss');
 
+require('laravel-mix-purgecss');
+
 /*
  |--------------------------------------------------------------------------
  | Mix Asset Management
@@ -17,4 +19,9 @@ mix.js('resources/js/app.js', 'public/js')
     .sass('resources/sass/app.scss', 'public/css')
     .options({
         postCss: [ tailwindcss('./tailwind.config.js') ],
+    })
+    .purgeCss({
+        enabled: mix.inProduction(),
+        folders: ['src', 'templates'],
+        extensions: ['html', 'js', 'php', 'vue'],
     });
